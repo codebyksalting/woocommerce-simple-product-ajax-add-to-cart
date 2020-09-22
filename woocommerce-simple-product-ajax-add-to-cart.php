eeab04d3ee8291221d7bd618351d56c89b64b3d4
KSalting <37995498+codebyksalting@users.noreply.github.com>
2020-09-22 14:22:50 +0800

    Update woocommerce-simple-product-ajax-add-to-cart.php

--- woocommerce-simple-product-ajax-add-to-cart.php
+++ woocommerce-simple-product-ajax-add-to-cart.php
@@ -6,7 +6,7 @@
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            Kendrick Salting
+ * Author:            KSalting
  * Author URI:        https://github.com/codebyksalting
  * License:           MIT
  * License URI:       https://opensource.org/licenses/MIT
